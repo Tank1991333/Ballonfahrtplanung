c1849659040b1e8de56eb00be8c7762ecd00f182
Tank1991333 <123tank456@gmail.com>
2026-09-18 10:29:54 +0200

    Refactor tile layer initialization in app.js

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const startLon = parseFloat(document.getElementById('lon').value) || 16.00930;
 // Karte im passenden dunklen Design laden (CartoDB DarkMatter)
 const map = L.map('map', { zoomControl: true }).setView([startLat, startLon], 12);
 
-L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    maxZoom: 19,
-    attribution: '&copy; https://www.openstreetmap.org/copyrightOpenStreetMap</a>'
-  }
-).addTo(map);
+const tileLayer = L.tileLayer(
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+{
+maxZoom: 19,
+attribution: "&copy; OpenStreetMap"
+}
+);
+ 
+tileLayer.addTo(map);
 
 // Startmarker erzeugen (Verschiebbar)
 const startMarker = L.marker([startLat, startLon], { draggable: true }).addTo(map);
